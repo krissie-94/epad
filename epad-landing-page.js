df7bf5983bb4cd19766692b5830920becc920800
krissie-94 <81246356+krissie-94@users.noreply.github.com>
2021-11-30 11:12:12 -0600

    Update epad-landing-page.js

--- epad-landing-page.js
+++ epad-landing-page.js
@@ -113,3 +113,22 @@ healthSide.addEventListener ('click', function(){
 // </div> `
 //     showuserInfo.innerHTML = userDetails;
 // })
+let featureSwitch = document.querySelectorAll('.feature-switcher');
+featureSwitch.forEach(item => {
+    item.addEventListener('click', function(){
+        featureSwitch.forEach(ele => {
+            ele.classList.remove('active-link');
+        });
+        this.classList.add('active-link');
+
+        let identity = this.id;
+        if(identity == 'healthcenterview'){
+            document.querySelector('#regular-users').classList.add('display-none');
+            document.querySelector('#hospital').classList.remove('display-none');
+        }else if(identity == 'userview'){
+            document.querySelector('#hospital').classList.add('display-none');
+            document.querySelector('#regular-users').classList.remove('display-none');
+        }
+
+    });
+});
